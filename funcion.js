3ec6f141481602aa27a4aef5f5a464fde5d275ed
mateozapata597 <mateobetancur1162620@correo.itm.edu.co>
2026-05-28 15:08:31 -0500

    Add files via upload
    clientes

---
+++ funcion.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    document.getElementById("modal-bienvenida").style.display = "flex";
+};
+
+function cerrarModal() {
+    document.getElementById("modal-bienvenida").style.display = "none";
+}
